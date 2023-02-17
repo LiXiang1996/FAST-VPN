8054fadcd475c9e95de67b7fe31aba6e22c5153b
LiXiang <1905710655@qq.com>
2023-02-17 16:33:50 +0800

    firebase

--- app/src/main/java/com/example/test/App.kt
+++ app/src/main/java/com/example/test/App.kt
@@ -5,6 +5,13 @@ import android.app.Application
 import android.content.Context
 import com.example.test.ui.activity.MainActivity
 import com.github.shadowsocks.Core
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.ktx.initialize
+import com.google.firebase.remoteconfig.FirebaseRemoteConfig
+import com.google.firebase.remoteconfig.ktx.remoteConfig
+
+@SuppressLint("StaticFieldLeak")
+val remoteConfig: FirebaseRemoteConfig = Firebase.remoteConfig
 
 class App : Application() {
 
@@ -13,9 +20,21 @@ class App : Application() {
         var context: Context? = null
             private set
     }
+
     override fun onCreate() {
         super.onCreate()
         context = applicationContext
         Core.init(this, MainActivity::class)
+        Firebase.initialize(this)
+        getRemoteConfig()
+    }
+
+    private fun getRemoteConfig() {
+        remoteConfig.fetchAndActivate()
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    remoteConfig.getString("axxxxxx")
+                }
+            }
     }
 }
